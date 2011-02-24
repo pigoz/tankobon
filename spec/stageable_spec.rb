53eec8780a71a3f3e446bd8e9ad2bcdb130f9372
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2011-02-24 22:14:27 +0100

    dried up the anonymous class creation

--- spec/stageable_spec.rb
+++ spec/stageable_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe Tankobon::Stageable do
-  # before(:each) do
-  #   FileUtils.mkdir_p 'test'
-  # end
-  # 
-  # after(:each) do
-  #   FileUtils.rm_rf 'test'
-  # end
+  before(:each) do
+    FileUtils.mkdir_p 'test'
+  end
+  
+  after(:each) do
+    FileUtils.rm_rf 'test'
+  end
+  
+  klass = Class.new(Tankobon::Stageable) do
+    def stage_root
+      Pathname.new("test")
+    end
+  end
   
   it "should stage to the correct directory" do
     Tankobon::Stageable.new("foo").stage.should == 
@@ -21,31 +27,16 @@ describe Tankobon::Stageable do
   end
   
   it "should return the StagedDirectory when calling to_stage" do
-    klass = Class.new(Tankobon::Stageable) do
-      def stage_root
-        Pathname.new("test")
-      end
-    end
     res = klass.new("foo2").to_stage
     res.class.should == Tankobon::StagedDirectory
   end
   
   it "should create the staging root directory when calling to_stage" do
-    klass = Class.new(Tankobon::Stageable) do
-      def stage_root
-        Pathname.new("test")
-      end
-    end
     klass.new("foo2").to_stage
     File.should exists 'test'
   end
   
   it "should delete the staging directory when calling to_stage" do
-    klass = Class.new(Tankobon::Stageable) do
-      def stage_root
-        Pathname.new("test")
-      end
-    end
     klass.new("foo2").to_stage
     File.should_not exists 'test/foo2'
   end
